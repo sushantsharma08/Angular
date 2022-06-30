fe2dc6b1136ca3e67506b64ecef6fe5ac5121475
Sushant Sharma <ssushantlb08@gmail.com>
2022-06-30 13:26:51 +0530

    Initial commit

---
+++ YT-tuto/src/app/components/test/test.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { bufferToggle } from 'rxjs';
+
+@Component({
+  selector: 'app-test',
+  template: `
+              <h2>welcome {{Name}}</h2>
+              <h2>Hello title</h2>
+              <h2 [style.color]="'orange'">Hello title</h2>
+              <h2 [style] = "titleStyles">Hello title</h2>
+              <button (click)="onClick()">button</button>
+              {{Space}}
+              `,
+  styles: []
+})
+export class TestComponent implements OnInit {
+  public Name = "sushant";
+  public Space = "";
+  constructor() { }
+
+  public titleStyles = {
+    color : "blue",
+    fontStyle : "italic"
+  }
+  onClick(){
+    console.log("hello")
+     this.Space = "hello";
+  }
+  ngOnInit(): void {
+  }
+
+}
