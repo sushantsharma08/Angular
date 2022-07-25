dd4b93f2217c7f25d774fd6a622ff90577ee4f91
Sushant Sharma <ssushantlb08@gmail.com>
2022-07-25 13:17:41 +0530

    basic form structure

---
+++ tdf/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'tdf';
+  topics = ['angular','react','veu'];
+  options = ['js','python','root'];
+}
